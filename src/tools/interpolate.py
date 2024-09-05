309949addfabac47bf3f06bfbf7f9d4857026697
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2024-09-05 09:15:48 +0200

    add: comparison of powers

--- src/tools/interpolate.py
+++ src/tools/interpolate.py
@@ -2,6 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 from scipy.constants import lambda2nu, c
 import toml
+import numpy as np
 
 def save_coefficients_to_csv(coefficients, filename):
     with open(filename, 'w', newline='') as csvfile:
@@ -41,15 +42,28 @@ if __name__ == "__main__":
     dir = "input/reflectivity/"
     names = ["braggSiN", "braggBN", "gmrSiN", "gmrBN"]
     labels = [r"Bragg SiN", r"Bragg BN", r"GMR SiN", r"GMR BN"] 
+    names = ["braggSiN", "gmrSiN"]
+    labels = [r"Bragg SiN", r"GMR SiN"] 
     config = toml.load('input/si_units.toml')
     P = config['P']
     m = config['m']
     t_rel = m*c**2/P
     f_0 = lambda2nu(1064e-9)
+    f_0 = 283.2e12
     cnt = 0
+    plt.figure(figsize=(4, 3))
     for i, n in enumerate(names):
       points = load_points_from_csv(dir, n)
-      plot_points(points, n, labels[i])
+      # plot_points(points, n, labels[i])
+      print(names[i])
       points = rescale_points(points, 1/f_0)
       save_coefficients_to_csv(points, "input/reflectivity/"+ n +"_f.csv")
       cnt += 1
+      print([p[0] for p in points])
+      plt.plot(np.array([p[0] for p in points]), [p[1] for p in points], label=labels[i])
+
+    plt.xlabel(r"$\omega/\omega_0$")
+    plt.ylabel(r"$\alpha_1(\omega)$")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("media/reflectivity/spectral_comparison.pdf")
