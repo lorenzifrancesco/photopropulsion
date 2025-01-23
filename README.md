2cb675a9ea97602207518a4b8e67532d1b3f2faa
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2025-01-23 18:24:48 +0100

    add: plotting with final parameters

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # photopropulsion
 A package to solve the relativistic Newton equation of a sail subjected to radiation pressure.
 Core numerics (RK4) is made in Rust, and simulation management is done in Python.
+
+```params.toml``` is utilized to set the SI parameters of the simulation, and ```config.toml``` is utilized to pass the normalized parameters to the Rust code.
--- src/interstellar.py
+++ src/interstellar.py
@@ -19,6 +19,9 @@ def plot_interstellar(todo = ["eta", "pow"],
                       ratio = True,
                       special = ""
                       ):
+  if ratio: 
+    assert not override 
+     
   if ratio:
     ylabel = r'$\Delta v / \Delta v_S$'
   else:
@@ -47,6 +50,10 @@ def plot_interstellar(todo = ["eta", "pow"],
                 r'$\mathrm{M}_{S2}$',
                 r'$\mathrm{M}_{S3}$',
                 r'$\mathrm{S}$',]
+  panels_pow = [r'(a)', r'(c)']
+  panels_eta = [r'(b)', r'(d)']
+  panel_pow = panels_pow[0]
+  panel_eta = panels_eta[0]
   if ratio:
     label_list = label_list[:-1]
     mode_range = mode_range[:-1]
@@ -55,6 +62,11 @@ def plot_interstellar(todo = ["eta", "pow"],
                 'b', 
                 'm', 
                 'orange']
+    panel_pow = panels_pow[1]
+    panel_eta = panels_eta[1]
+    ylims = [0.0, 9.3]
+  else:
+    ylims = [0, 0.35]
   n_simulations = len(label_list)
   file = "auto.csv"
   output = "results/"
@@ -89,12 +101,13 @@ def plot_interstellar(todo = ["eta", "pow"],
     results_matrix = np.load("results/v_eta.npy")
     print("Plotting...")
     print(np.shape(results_matrix))
+    n_last_result = np.shape(results_matrix)[1]
     # single line
-    plt.figure(figsize=(3, 2.5))
+    plt.figure(figsize=(3.5, 2.8))
     np.set_printoptions(precision=10)
     if ratio:
       for i in range(np.shape(results_matrix)[1]-1):
-        results_matrix[:, i] = results_matrix[:, i] / results_matrix[:, 4]
+        results_matrix[:, i] = results_matrix[:, i] / results_matrix[:, n_last_result-1]
     for (j, alpha) in enumerate(mode_range):
         plt.plot(eta_range, results_matrix[:, j],
                   color=color_list[j], ls=ls_list[j], lw=1.5, label=label_list[j])
@@ -103,7 +116,7 @@ def plot_interstellar(todo = ["eta", "pow"],
     td_fom = 2 * l.p_0 * alpha1/(3e8 * (l.sail_mass *(1+eta_range))) * l.t_f / c
     td_fom[td_fom > 1] = np.nan
     if ratio:
-        td_fom = td_fom / results_matrix[:, 4]
+        td_fom = td_fom / results_matrix[:, n_last_result-1]
     plt.plot(eta_range, td_fom,
               color=color_list[n_simulations], ls=ls_list[n_simulations], lw=1.5, label=r'${\mathrm{NR}}$')
 
@@ -112,12 +125,18 @@ def plot_interstellar(todo = ["eta", "pow"],
     num_xticks = 5
     # plt.legend(labelspacing=0.1)
     plt.tight_layout()
+    plt.ylim(ylims)
+    plt.text(0.5, 0.95, panel_eta, 
+         horizontalalignment='center', 
+         verticalalignment='center', 
+         transform=plt.gca().transAxes, 
+         fontsize=12)
     plt.savefig("media/v_eta"+special+".pdf")
     print("Done eta.")
 
   ## POWER section =====================
   if "pow" in todo:
-    power_range = np.linspace(1, 100, 40, dtype=np.float64) * 1e9
+    power_range = np.linspace(10, 100, 25, dtype=np.float64) * 1e9
     l = simulation.Launch()
     l.eta = 0.0
     if not os.path.exists("results/v_pow.npy") or override:
@@ -163,12 +182,18 @@ def plot_interstellar(todo = ["eta", "pow"],
 
     plt.xlabel(r'$P_0$')
     plt.ylabel(ylabel)
+    plt.ylim(ylims)
     num_xticks = 5
     xtick_positions = np.linspace(
         power_range.min(), power_range.max(), num_xticks)
     plt.gca().ticklabel_format(style='sci', axis='x', scilimits=(3, 0))
     plt.legend(labelspacing=0.1)
     plt.tight_layout()
+    plt.text(0.5, 0.95, panel_pow, 
+         horizontalalignment='center', 
+         verticalalignment='center', 
+         transform=plt.gca().transAxes, 
+         fontsize=12)
     plt.savefig("media/v_pow"+special+".pdf")
     print("Done power.")
     
@@ -180,7 +205,14 @@ if __name__ == "__main__":
     else:
       special = ""
       ratio = False
+    #
     plot_interstellar(todo = ["eta", "pow"],
-                      override = True,
-                      ratio = ratio, 
-                      special = special)
+                override = False,
+                ratio = ratio, 
+                special = special)
+    
+  # make this out of the for loop for overriding the values in the files
+  # plot_interstellar(todo = ["eta", "pow"],
+  #                   override = True,
+  #                   ratio = False, 
+  #                   special = "")
